bbd4bbc6b948f2c1beb5a39292c035d9db3f2041
Aerll <31746984+Aerll@users.noreply.github.com>
2023-09-29 08:22:56 +0200

    Revert "fix formatting"
    This reverts commit 95e55117c519e82bd4924dcba6e2c5448c724e24.

--- src/rulesgen.cpp
+++ src/rulesgen.cpp
@@ -39,7 +39,7 @@ void RulesGen::exec(std::vector<AutoMapper>& automappers, const std::filesystem:
     std::ofstream rulesFile(outputFile.string());
     if (rulesFile.is_open()) {
         for (auto& automapper : automappers) {
-            rulesFile << "[" << automapper.name << "]\n\n";
+            rulesFile << "[" << automapper.name << "]\n";
 
             for (size_t run_index = 0; run_index < automapper.runs.size(); ++run_index) {
                 auto& run = automapper.runs[run_index];
@@ -53,8 +53,7 @@ void RulesGen::exec(std::vector<AutoMapper>& automappers, const std::filesystem:
                         rulesFile << "NewRun\n";
                     if (run.overrideLayer)
                         rulesFile << "NoLayerCopy\n";
-                    if (run_index != 0)
-                        rulesFile << '\n';
+                    rulesFile << '\n';
 
                     for (auto& rule : run.rules) {
                         if (i == 0) // optimize only once
